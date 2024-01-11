76779576beb36af6d28f1981799386fd02d80d91
xujj@huaun.com <xujj@huaun.com>
2024-01-11 17:42:40 +0800

    异步删除

--- chrome/chrome.go
+++ chrome/chrome.go
@@ -65,17 +65,20 @@ func (c *Chrome) Close() {
 	for _, v := range c.cancels {
 		v()
 	}
-	for {
-		time.Sleep(3 * time.Second)
-		MainInsp.Print(LEVEL_DEBUG, Text("尝试删除文件"+c.tmpPath))
-		err := os.RemoveAll(c.tmpPath)
-		if err != nil {
-			MainInsp.Print(LEVEL_DEBUG, Text("删除文件失败,即将重试"+c.tmpPath+err.Error()))
-		} else {
-			MainInsp.Print(LEVEL_DEBUG, Text("删除文件成功"+c.tmpPath))
-			break
+	go func() {
+		for {
+			time.Sleep(1 * time.Second)
+			MainInsp.Print(LEVEL_DEBUG, Text("尝试删除文件"+c.tmpPath))
+			err := os.RemoveAll(c.tmpPath)
+			if err != nil {
+				MainInsp.Print(LEVEL_DEBUG, Text("删除文件失败,即将重试"+c.tmpPath+err.Error()))
+			} else {
+				MainInsp.Print(LEVEL_DEBUG, Text("删除文件成功"+c.tmpPath))
+				break
+			}
 		}
-	}
+	}()
+
 }
 func init() {
 	InitLogger()
